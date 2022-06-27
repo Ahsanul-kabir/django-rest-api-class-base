098e4d09df270fe81ef39cab2e84063064e8e762
Ahsanul-kabir <ahsanul15-8635@diu.edu.bd>
2022-06-27 12:37:46 +0600

    urls updated

--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('data-view/<int:pk>/', views.dataView, name='dataview'),
-    path('data-add', views.dataAdd, name='dataadd'),
+    path('data-add/', views.dataAdd, name='dataadd'),
     path('update-data/<int:pk>/', views.dataUpdate, name='dataupdate'),
     path('delete-data/<int:pk>/', views.datadelete, name='datadelete'),
 ]
